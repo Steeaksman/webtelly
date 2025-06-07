bbfbbd64fe806a37870a17db43d256810687a022
Donny <nstokes142@gmail.com>
2025-06-06 22:40:57 -0700

    3rd commit

--- pages/video.vue
+++ pages/video.vue
@@ -4,7 +4,7 @@
   <!-- <div class="min-h-screen  relative bg-cover bg-bottom" style="background-image: url('https://tellysite.s3.us-west-1.amazonaws.com/RealBlue+Concert+background_2.jpg')"> -->
   <div class="p-8">
     <h1 class="text-2xl  mb-4 text-white">Make Me Rainbows</h1>
-    <VideoPlayer
+    <videoPlayer
       src="https://tellysite.s3.us-west-1.amazonaws.com/Make+me+rainbows.mp4"
       poster="https://tellysite.s3.us-west-1.amazonaws.com/thumbnailRainbows2.jpg"
       type="video/mp4"
@@ -14,5 +14,5 @@
 </template>
 
 <script setup>
-import VideoPlayer from '/components/videoPlayer.vue'
+import VideoPlayer from '~/components/videoPlayer.vue'
 </script>
