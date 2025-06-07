ebaea7a9949fbcf24a6bc7516d1a0c5d77fcd723
Donny <nstokes142@gmail.com>
2025-06-06 23:05:24 -0700

    5com

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="pb-2 min-h-screen bg-fuchsia-500 ">
      <div class="container mx-auto px-6 lg:px-0 py-2 text-center">
-<div class="md:flex justify-between">
+<div class="md:flex justify-around">
     <div><h2 class="md:text-8xl text-2xl font-bold text-gray-100 mb-4 md:pt-36">Terri Yates<br></h2>
 
  <h2 class="mb-4 md:text-3xl text-2xl  text-gray-100">
